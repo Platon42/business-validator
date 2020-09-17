b2057fac08f113ebf8587a0854dcf476cd759aa1
kotol@icloud.com <kotol@icloud.com>
2020-09-17 20:54:01 +0300

    added hibernate

--- src/main/java/org/hash/validator/dto/InnValidatorRequest.java
+++ src/main/java/org/hash/validator/dto/InnValidatorRequest.java
@@ -1,6 +1,10 @@
 package org.hash.validator.dto;
 
 import lombok.Data;
+import org.hash.validator.dto.company.Company;
+
+import java.util.ArrayList;
+import java.util.HashSet;
 
 @Data
 public class InnValidatorRequest {
